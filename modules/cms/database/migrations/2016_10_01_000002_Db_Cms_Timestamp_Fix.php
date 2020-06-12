1f6c5cec04c3b2a960ebf4fe1eea1183dba4e351
Romain 'Maz' BILLOIR <contact@maz.digital>
2020-06-13 01:14:53 +0200

    Fix typo

--- modules/cms/database/migrations/2016_10_01_000002_Db_Cms_Timestamp_Fix.php
+++ modules/cms/database/migrations/2016_10_01_000002_Db_Cms_Timestamp_Fix.php
@@ -6,7 +6,7 @@ use October\Rain\Database\Updates\Migration;
  * This migration addresses a MySQL specific issue around STRICT MODE.
  * In past versions, Laravel would give timestamps a bad default value
  * of "0" considered invalid by MySQL. Strict mode is disabled and the
- * the timestamps are patched up. Credit for this work: Dave Shoreman.
+ * timestamps are patched up. Credit for this work: Dave Shoreman.
  */
 class DbCmsTimestampFix extends Migration
 {
